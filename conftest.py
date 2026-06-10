602911e1d4cb31275446418b306de4246fd8de1c
TryDkg <bmi92mbs@gmail.com>
2026-06-10 22:39:16 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,18 +1,3 @@
-import pytest
-
-@pytest.fixture(scope="function")
-def browser_context_args(browser_context_args):
-    """Настройки контекста браузера для всех UI-тестов."""
-    return {
-        **browser_context_args,
-        "viewport": {"width": 1920, "height": 1080},
-        "locale": "en-US",
-    }
-
-@pytest.fixture(scope="function")
-def ui_base_url():
-    return "https://www.saucedemo.com"
-
 @pytest.fixture(scope="function")
 def api_base_url():
-    return "https://reqres.in/api"
+    return "https://jsonplaceholder.typicode.com"
