fbb7ecb69958b7a5d2f72f7cc4267f4c7a38e172
TryDkg <bmi92mbs@gmail.com>
2026-06-10 22:39:04 +0300

    Update test_users.py

--- tests/api/test_users.py
+++ tests/api/test_users.py
@@ -8,20 +8,17 @@ class TestUsers:
 
     @allure.title("Получение одного пользователя и валидация схемы")
     @pytest.mark.api
-    def test_get_single_user(self, reqres_client):
-        response = reqres_client.get_single_user(2)
+    def test_get_single_user(self, api_client):
+        response = api_client.get_single_user(1)
         assert response.status_code == 200
         body = response.json()
         validate(instance=body, schema=SINGLE_USER_SCHEMA)
-        assert body["data"]["id"] == 2
-        assert body["data"]["first_name"] is not None
 
     @allure.title("Создание пользователя и проверка ответа")
     @pytest.mark.api
-    def test_create_user(self, reqres_client):
-        response = reqres_client.create_user("morpheus", "leader")
+    def test_create_user(self, api_client):
+        response = api_client.create_user("John", "john@example.com")
         assert response.status_code == 201
         body = response.json()
         validate(instance=body, schema=CREATE_USER_SCHEMA)
-        assert body["name"] == "morpheus"
-        assert body["job"] == "leader"
+        assert body["name"] == "John"
