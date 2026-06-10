ceff415d414014568d4f346950d02156d03fed7c
TryDkg <bmi92mbs@gmail.com>
2026-06-10 22:38:35 +0300

    Update reqres_client.py

--- api/reqres_client.py
+++ api/reqres_client.py
@@ -1,20 +1,20 @@
 import requests
 import allure
 
-class ReqresClient:
+class JsonPlaceholderClient:
     def __init__(self, base_url: str):
         self.base_url = base_url
         self.session = requests.Session()
 
-    @allure.step("GET /users?page={page}")
-    def get_users(self, page: int = 1):
-        return self.session.get(f"{self.base_url}/users", params={"page": page})
+    @allure.step("GET /users")
+    def get_users(self):
+        return self.session.get(f"{self.base_url}/users")
 
     @allure.step("POST /users")
-    def create_user(self, name: str, job: str):
+    def create_user(self, name: str, email: str):
         return self.session.post(
             f"{self.base_url}/users",
-            json={"name": name, "job": job}
+            json={"name": name, "email": email}
         )
 
     @allure.step("GET /users/{user_id}")
